9a8012c189980809b89634056526f68da52985db
Kaio Menezes <73968667+kaiomenezes57@users.noreply.github.com>
2026-08-02 14:03:08 -0300

    feat: update home page

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ButtonComponent } from "../../../shared/components/button/button";
+import { Input } from "../../../shared/components/input/input/input";
+import { PollService } from '../../poll/services/poll-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, Input],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-  onSubmit(): void {
-    console.log('Botão clicado!');
+  private readonly router = inject(Router);
+  private pollId = '';
+
+  setValue(value: string): void {
+    this.pollId = value;
+  }
+
+  submit(): void {
+    if (!this.pollId.trim()){
+      alert('Please enter a valid poll ID');
+      return;
+    }
+
+    this.router.navigate(['/poll', this.pollId]);
   }
 }
---
+++ src/app/shared/components/input/input/input.ts
@@ -0,0 +1,21 @@
+import { Component, input, output } from '@angular/core';
+
+type InputTypes = 'text' | 'email' | 'password' | 'search' | 'tel' | 'url';
+
+@Component({
+  selector: 'app-input',
+  imports: [],
+  templateUrl: './input.html',
+  styleUrl: './input.css',
+})
+export class Input {
+  type = input<InputTypes>('text');
+  placeHolder = input<string>('Enter text');
+  
+  value = output<string>();
+
+  onChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.value.emit(inputElement.value);
+  }
+}
